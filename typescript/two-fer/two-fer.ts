8136dfa5ad4262d1d4a7e48b28d7ab47be40f334
danembb <dbrophy720@gmail.com>
2023-06-13 17:24:08 -0600

    Add two-fer exercise

---
+++ typescript/two-fer/two-fer.ts
@@ -0,0 +1,9 @@
+/**
+ * This stub is provided to make it straightforward to get started.
+ */
+
+// if name exists then name, else 'you'
+
+export function twoFer(name = 'you'): string {
+  return `One for ${name}, one for me.`;
+};

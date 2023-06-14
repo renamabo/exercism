163cabb210250a5d7c3a4d2518064e7b3eaed452
danembb <dbrophy720@gmail.com>
2023-06-13 18:04:04 -0600

    Exercise 3: resistor color duo start

---
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -0,0 +1,15 @@
+// Restistors: color-coded bands denoting resistance values:
+// Each band has a 1. position and 2. numeric value
+// Each color on a band 'maps' to a single number. i.e. brown (1) + green (5) = 15.
+
+// create a program that will take in the values of the bands and output numbers (so each
+// time i run, i will not need to remember the numbers.)
+
+// bands = { blah, blah, blah}
+// def decodedValue(band_1, band_2)
+//  return 
+// end
+let bands = { 'black': 0, 'brown': 1, 'red': 2, 'orange': 3, 'yellow': 4, 'green': 5, 'blue': 6, 'violet': 7, 'grey': 8, 'white': 9}
+
+export function decodedValue(bands: any) {
+};
